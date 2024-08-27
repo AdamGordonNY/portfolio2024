678d5f15aff5a06f0f8e6e8eddfcc7cac4f6382a
adam <adamg42685@gmail.com>
2024-08-27 05:02:43 -0400

    changed bg to 200

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Landing from "@/components/Landing";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-full  flex-col items-center  justify-center bg-dark-100 ">
+    <main className="flex min-h-screen min-w-full  flex-col items-center  justify-center bg-dark-200 ">
       <Landing />
     </main>
   );
